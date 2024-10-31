5cb3a1075c0d31b0fe8aa52c64567ca9718dc3c8
Hamze Ghorbani Koujani <s325864@studenti.polito.it>
2024-10-31 17:54:58 +0100

    LAB 3 is all done.

--- core/elements.py
+++ core/elements.py
@@ -1,4 +1,10 @@
 import json
+from signal import signal
+
+import matplotlib.pyplot as plt
+from networkx.classes import nodes
+from core import parameters
+
 #from core.utils import example_func
 
 class Signal_information(object):
@@ -53,89 +59,150 @@ class Signal_information(object):
 
 
 class Node(object):
-    def __init__(self):
-        pass
+    def __init__(self, input_dict):
+        self._label = input_dict['label']
+        self._position = tuple(input_dict['position'])
+        self._connected_nodes = input_dict['connected_nodes']
+        self._successive = dict()
 
     @property
     def label(self):
-        pass
+        return self._label
 
     @property
     def position(self):
-        pass
+        return self._position
 
     @property
     def connected_nodes(self):
-        pass
+        return self._connected_nodes
 
     @property
     def successive(self):
-        pass
+        return self._successive
 
     @successive.setter
-    def successive(self):
-        pass
+    def successive(self, new_successive):
+        self._successive = new_successive
 
-    def propagate(self):
-        pass
+    def propagate(self, signal_information):
+        signal_information.update_path()
+        if signal_information.path:
+            next_node = signal_information.path[0]
+            for line_label, line in self._successive.items():
+                if line_label.find(next_node)!=-1:
+                    line.propagate(signal_information)
 
 
 class Line(object):
-    def __init__(self):
-        pass
+    def __init__(self, label, length:float):
+        self._label = label
+        self._length = length
+        self._successive = {}
 
     @property
     def label(self):
-        pass
+        return self._label
 
     @property
     def length(self):
-        pass
+        return self._length
 
     @property
     def successive(self):
-        pass
+        return self._successive
 
     @successive.setter
-    def successive(self):
-        pass
+    def successive(self, new_successive):
+        self._successive = new_successive
 
-    def latency_generation(self):
-        pass
+    def latency_generation(self,signal_information):
+        latency_generated = self._length / (parameters.c * 2 / 3)
+        signal_information.update_latency(latency_generated)
 
-    def noise_generation(self):
-        pass
+    def noise_generation(self, signal_information):
+        noise_generated = 1e-9 * signal_information.signal_power * self._length
+        signal_information.update_noise_power(noise_generated)
 
-    def propagate(self):
-        pass
+    def propagate(self, signal_information):
+        self.latency_generation(signal_information)
+        self.noise_generation(signal_information)
+        self._successive[signal_information.path[0]].propagate(signal_information)
 
 
 class Network(object):
-    def __init__(self):
-        pass
+    def __init__(self, json_file_path):
+        self._nodes = {}
+        self._lines = {}
+        with open(json_file_path,"r") as f:
+            nodes_data = json.load(f)
+        for node_label, node_info in nodes_data.items():
+            node_info['label'] = node_label
+            node = Node(node_info)
+            self._nodes[node.label] = node
+        for node_label1, node1 in self._nodes.items():
+            for node_label2 in node1.connected_nodes:
+                line_label = node_label1+node_label2
+                line_length = self._calculate_distance(node1.position, self._nodes[node_label2].position)
+                line = Line(line_label, line_length)
+                self._lines[line.label] = line
+            # nodes_to_pass.append(node_label1) # save the nodes that all the links to them are calculated to surpass them.
+
+
+    def _calculate_distance(self, pos1, pos2):
+        return ((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2) ** 0.5
 
     @property
     def nodes(self):
-        pass
+        return self._nodes
 
     @property
     def lines(self):
-        pass
+        return self._lines
 
     def draw(self):
-        pass
+        nodes_to_pass=[]
+        for node_label, node in self._nodes.items():
+            for next_node_label in node.connected_nodes:
+                if next_node_label in nodes_to_pass:
+                    continue    #to avoid drawing repetitive lines
+                node2 = self._nodes[next_node_label]
+                plt.plot([node.position[0],node2.position[0]],[node.position[1],node2.position[1]],'b')
+            nodes_to_pass.append(node_label)
+            plt.plot(node.position[0],node.position[1],'go', markersize=15)
+            plt.text(node.position[0],node.position[1],node_label)
+        plt.title('Network')
+        plt.show()
+
 
     # find_paths: given two node labels, returns all paths that connect the 2 nodes
     # as a list of node labels. Admissible path only if cross any node at most once
     def find_paths(self, label1, label2):
-        pass
+        possible_paths = []
+        stack = [[label1]]
+        while stack:
+            path = stack.pop()
+            if path[-1]==label2:    # Current node
+                possible_paths.append(path)
+            else:
+                for next_node in self._nodes[path[-1]].connected_nodes:
+                    if next_node not in path:
+                        new_path = path.copy()
+                        new_path.append(next_node)
+                        stack.append(new_path)
+        return possible_paths
 
     # connect function set the successive attributes of all NEs as dicts
     # each node must have dict of lines and viceversa
     def connect(self):
-        pass
+        for node1_label, node in self._nodes.items():
+            for next_node in node.connected_nodes:
+                line_label = node1_label + next_node
+                node.successive[line_label] = self._lines[line_label]
+                self._lines[line_label].successive[next_node] = self._nodes[next_node]
 
     # propagate signal_information through path specified in it
     # and returns the modified spectral information
     def propagate(self, signal_information):
-        pass
+        first_node = signal_information.path[0]
+        self._nodes[first_node].propagate(signal_information)
--- tasks/lab3_network_main.py
+++ tasks/lab3_network_main.py
@@ -1,9 +1,11 @@
 import json
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
-from core.elements import Network
+from core.elements import Network, Signal_information
 
 # Exercise Lab3: Network
 
@@ -17,4 +19,41 @@ file_input = INPUT_FOLDER / 'nodes.json'
 # Convert this dataframe in a csv file called 'weighted_path' and finally plot the network.
 # Follow all the instructions in README.md file
 
-net = Network()
+def main():
+    net = Network(file_input)
+    #print(net.nodes)
+    #print(net.lines)
+    #net.draw()
+    net.connect()
+    all_possible_paths = []
+    all_possible_paths_string = []
+    acc_latency =[]
+    acc_noise = []
+    SNR = []
+    signal_power = 1e-3
+    # Find all possible paths
+    for node1_label, node1 in net.nodes.items():
+        for node2_label, node2 in net.nodes.items():
+            if node1_label == node2_label:
+                continue
+            paths = (net.find_paths(node1_label,node2_label))
+            path_string = ['->'.join(i) for i in paths]
+            all_possible_paths.extend(paths)
+            all_possible_paths_string.extend(path_string)
+            for path in paths:
+                signal = Signal_information(signal_power,path.copy())
+                net.propagate(signal)
+                acc_latency.append(signal.latency)
+                acc_noise.append(signal.noise_power)
+                SNR.append(10*math.log10(signal.signal_power/signal.noise_power))
+    data = pd.DataFrame({'Path':all_possible_paths_string,'Accumulated Latency':acc_latency,
+                         'Accumulated Noise':acc_noise, 'SNR':SNR})
+    data.to_csv(INPUT_FOLDER/'weighted_path.csv',index=False)
+    net.draw()
+    #print(paths)
+    print(all_possible_paths)
+            #for path in possible_paths:
+
+
+if __name__ == '__main__':
+    main()
